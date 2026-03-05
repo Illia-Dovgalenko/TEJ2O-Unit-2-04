175a44da8ba31aa9ae308afa786b37908f0f003f
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-03-05 02:17:47 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,15 +1,19 @@
 """
-Created by: Illia
-Created on: MAR 2026
-This program: Temperature
+Created by: Mr. Coxall
+Created on: Sep 2020
+This module simulates a 6 sided dice
 """
 
 from microbit import *
+import random
+
+# our variable for a random number
+random_number = 0
 
 display.clear()
 display.show(Image.HAPPY)
 
 while True:
-    if button_a.was_pressed():
-        display.scroll(temperature() + "C")
-
+    if button_a.is_pressed():
+        random_number = random.randint(1, 6)
+        display.show(str(random_number))
